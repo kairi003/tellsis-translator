77599d9110fc6f0754024d280be3c9cc11f82dc5
kairi003 <kairi.satellite@gmail.com>
2021-01-15 23:34:11 +0900

    [update] refine

---
+++ docs/script/main.js
@@ -0,0 +1,16 @@
+(async () => {
+  const proms = ['data/translate.json', 'data/proper-noun.json'].map(url => fetch(url).then(r => r.json()));
+  proms.push(new Promise(resolve => window.addEventListener('DOMContentLoaded', resolve)));
+  const [translate, properNounData] = await Promise.all(arr);
+  const tamil = document.getElementById('tamil');
+  const properNoun = document.getElementById('properNoun');
+  properNoun.value = properNounData.join('\n');
+  const inputTextarea = document.querySelectorAll('textarea.input');
+  inputTextarea.forEach(elm => elm.addEventListener('input', event => {
+    const input_str = event.currentTarget.value;
+    inputTextarea.forEach(elm => elm.value = input_str);
+    const properNounSet = new Set(properNoun.value.match(/\w+/g));
+    tamil.value = input_str.replace(/\w+/g, word =>
+      properNounSet.has(word) ? word : word.replace(/[A-Za-z]/g, c => translate[c]));
+  }));
+})();
